7c92d77bc4bf783717a2999380942e4540af913a
joon <essueweb@gmail.com>
2025-01-09 15:21:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 1. 빌드
+mvn clean install
+
+# 2. 서버 실행
+mvn tomcat7:run
+
+# 3. 페이지 열기
+http://localhost:8080
---
+++ src/main/java/com/simpleserver/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.simpleserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "hello";
+    }
+}
